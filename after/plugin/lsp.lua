82b90919fc71be559ec21e7975dbd9f0256a22bb
Danniel Rolfe <talkto@dannielrolfe.com>
2023-05-10 16:48:28 -0700

    Fix error

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -10,12 +10,25 @@ lsp.ensure_installed({
 })
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp SelectBehavior Select}
-local cmp_mappings = lsp.default.cmp_mappings({
-	'<C-p>' = cmp.mapping.select_prev_item(cmp_select),
-	'<C-n>' = cmp.mapping.select_next_item(cmp_select),
-	'<C-y>' = cmp.mapping.confirm({ select = true }),
-	'<C-Space>' = cmp.mapping.complete()
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+	["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+	["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+	["<C-y>"] = cmp.mapping.confirm({ select = true }),
+	["<C-Space>"] = cmp.mapping.complete(),
 })
+
+lsp.set_preferences({
+	sign_icons = { }
+})
+
+lsp.on_attach(function(client, bufnr)
+	local opts = { buffer = bufnr, remap = false }
+
+	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+	vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.goto_next() end, opts)
+end)
 
 lsp.setup()
