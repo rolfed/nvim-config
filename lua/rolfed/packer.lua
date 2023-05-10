9aceeba63aba06af387853bea08b8e210d7e371b
Danniel Rolfe <talkto@dannielrolfe.com>
2023-05-10 13:00:41 -0700

    Add treesitter

--- lua/rolfed/packer.lua
+++ lua/rolfed/packer.lua
@@ -8,8 +8,12 @@ return require('packer').startup(function(use)
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
 
-	use({ 'rose-pine/neovim', as = 'rose-pine'})
-	vim.cmd('colorscheme rose-pine')
+	use({ 'rose-pine/neovim', as = 'rose-pine', config = function()
+		vim.cmd('colorscheme rose-pine')
+	end
+	})
+
+	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 
 end)
 
