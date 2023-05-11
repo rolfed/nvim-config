018ed63d8cc8a296ecf3f0869f18c4b88518a7e7
Danniel Rolfe <danniel.rolfe@experian.com>
2023-05-11 13:50:36 -0700

    Add better escape

--- lua/rolfed/remap.lua
+++ lua/rolfed/remap.lua
@@ -14,6 +14,7 @@ vim.keymap.set("n", "N", "Nzzzv")
 
 vim.keymap.set("x", "<leader>p", "\"_dp")
 
+-- Copy to clipboard
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+y")
