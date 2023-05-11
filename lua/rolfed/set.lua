06c9203002396a1950a40b2942e3ae97d24a6c7b
Danniel Rolfe <talkto@dannielrolfe.com>
2023-05-10 19:46:20 -0700

    Add additional mappings

--- lua/rolfed/set.lua
+++ lua/rolfed/set.lua
@@ -1,1 +1,37 @@
 vim.opt.guicursor = ""
+
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .."/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+
+vim.g.mapleader = ","
+
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+y")
+
+
+
