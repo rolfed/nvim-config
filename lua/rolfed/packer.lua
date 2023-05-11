4859d46943963c87dd0a0a33d28275622850ab6c
Danniel Rolfe <talkto@dannielrolfe.com>
2023-05-10 20:34:04 -0700

    Add tmux nvim navigator script

--- lua/rolfed/packer.lua
+++ lua/rolfed/packer.lua
@@ -18,6 +18,7 @@ use('nvim-treesitter/playground')
 use('theprimeagen/harpoon')
 use('mbbill/undotree')
 use('tpope/vim-fugitive')
+use('christoomey/vim-tmux-navigator')
 
 -- LSP ZERO
 use {
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -154,6 +154,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/dannielrolfe/.local/share/nvim/site/pack/packer/start/vim-fugitive",
     url = "https://github.com/tpope/vim-fugitive"
+  },
+  ["vim-tmux-navigator"] = {
+    loaded = true,
+    path = "/Users/dannielrolfe/.local/share/nvim/site/pack/packer/start/vim-tmux-navigator",
+    url = "https://github.com/christoomey/vim-tmux-navigator"
   }
 }
 
