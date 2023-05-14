69117f04794a8aac57d87516546e724b65b878f8
Danniel Rolfe <talkto@dannielrolfe.com>
2023-05-13 19:30:42 -0700

    Add cmp keybidnings

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -10,6 +10,7 @@ lsp.ensure_installed({
 
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
+-- cmp keymapings
 local cmp_mappings = lsp.defaults.cmp_mappings({
 	["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
 	["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
@@ -17,17 +18,38 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
 	["<C-Space>"] = cmp.mapping.complete(),
 })
 
+-- disable tab
+cmp_mappings['<Tabs>'] = nil
+cmp_mappings['<S-Tab>'] = nil
+
+lsp.setup_nvim_cmp({
+				mapping = cmp_mappings
+})
+
 lsp.set_preferences({
-	sign_icons = { }
+	sign_icons = {
+									error = 'E',
+									warn = 'W',
+									hint = 'H',
+									info = 'I'
+	}
 })
 
 lsp.on_attach(function(client, bufnr)
 	local opts = { buffer = bufnr, remap = false }
 
+	-- cmp go to definition
 	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+	-- cmp go to declaration
+	vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
+	-- cmp buffer viewe
 	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+
 	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+	-- view definition
 	vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.goto_next() end, opts)
+	-- rename / refactor
+	vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
 end)
 
 lsp.setup()
